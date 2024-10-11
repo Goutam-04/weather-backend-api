b02a94b090454665bbc3adeb2dca840c3d624623
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-10-11 21:12:41 +0530

    implemented rate-limiting feature for smoother use

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from 'dotenv';
 import weather from './routes/weather'
+import { rateLimit } from 'express-rate-limit'
+
+
 
 
 dotenv.config();
 const app=express()
 const port=process.env.PORT || 8080
+
+const limiter = rateLimit({
+	windowMs:  60 * 60 * 1000, // 1 hour
+	limit: 50, // Limit each IP to 100 requests per `window` (here, per 1 hour).
+})
+
 app.use(express.json());
+app.use(limiter)
 
 app.use('/api/',weather)
 
